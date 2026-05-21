4149fe6ba34d700fadc4824b8ee0d7f34550dd02
Abdel <darafital@gmail.com>
2026-05-21 20:04:28 +0100

    chambres partials edited

--- resources/views/themes/lasanta/partials/home/rooms.blade.php
+++ resources/views/themes/lasanta/partials/home/rooms.blade.php
@@ -1,34 +1,5 @@
-<!-- <section class="blog1 section-padding bg-lightbrown">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center mb-20">
-                <div class="section-subtitle">{{ $apartmentsSubtitle }}</div>
-                <div class="section-title">{{ $apartmentsTitle }}</div>
-            </div>
-        </div>
-        <div class="row">
-            @forelse(($homeRooms ?? collect()) as $room)
-                <div class="col-lg-4 col-md-6 mb-30">
-                    <div class="item">
-                        <div class="img">
-                            <img src="{{ media_url($room->main_image ?? null, 'themes/lasanta/img/restaurant/1.jpg') }}" class="img-fluid" alt="">
-                            <div class="cat">Appartement</div>
-                        </div>
-                        <div class="cont">
-                            <h4><a href="{{ route('rooms.show', $room->slug) }}">{{ method_exists($room, 't') ? $room->t('title') : $room->title }}</a></h4>
-                            <p>{{ \Illuminate\Support\Str::limit(strip_tags(method_exists($room, 't') ? $room->t('description') : ($room->description ?? '')), 120) }}</p>
-                        </div>
-                    </div>
-                </div>
-            @empty
-                <div class="col-12 text-center">Aucun appartement publié.</div>
-            @endforelse
-        </div>
-    </div>
-</section> -->
-
 <!-- Rooms 1 -->
-<section class="rooms1 section-padding bg-darkgray">
+<!-- <section class="rooms1 section-padding bg-darkgray">
     <div class="container">
         <div class="row mb-30">
             <div class="col-12 text-center">
@@ -39,16 +10,7 @@
                 @endphp
                 <div class="section-title white mb-0">{!! $titleHtml !!}</div>
             </div>
-            <!-- <div class="col-md-5">
-                <p>The experience elementum sesue the aucan vestibulum usto sapien rutrum volutan donec fermen lorem ipsum quisque sodales miss in the varius drana miss.</p>
-            </div> -->
-            <!-- <div class="col-md-2 d-flex justify-content-center justify-content-lg-end">
-                <div class="my-owl-nav"> <span class="my-prev-button">
-                        <i class="fa-light fa-angle-left" aria-hidden="true"></i>
-                    </span> <span class="my-next-button">
-                        <i class="fa-light fa-angle-right" aria-hidden="true"></i>
-                    </span> </div>
-            </div> -->
+           
         </div>
         <div class="row">
             <div class="col-md-12">
@@ -63,13 +25,7 @@
                         </div>
                         <div class="wrap">
                             <div class="cont">
-                                <!-- <div class="price">
-                                    @if(!empty($room->price_per_night))
-                                        {{ number_format($room->price_per_night, 0, ',', ' ') }} € <span>/ nuit</span>
-                                    @else
-                                        Sur devis <span>/ nuit</span>
-                                    @endif
-                                </div> -->
+                                
                                 <h3><a href="{{ route('rooms.show', $room->slug) }}">{{ method_exists($room, 't') ? $room->t('title') : $room->title }}</a></h3>
                                 <div class="details">
                                     @php
@@ -105,4 +61,47 @@
             </div>
         </div>
     </div>
-</section>
+</section> -->
+
+
+
+<section class="rooms3 section-padding bg-darkgray">
+        <div class="row" bis_skin_checked="1">
+            <div class="col-md-12 mb-25 text-center" bis_skin_checked="1">
+                <div class="section-subtitle" bis_skin_checked="1">{{ $apartmentsSubtitle }}</div>
+                @php
+                    $safeTitle = e($apartmentsTitle);
+                    $titleHtml = str_replace('&amp;', '<span>&amp;</span>', $safeTitle);
+                @endphp
+                <div class="section-title white" bis_skin_checked="1">{!! $titleHtml !!}</div>
+            </div>
+        </div>
+        <div class="row" bis_skin_checked="1">
+            <div class="col-md-12" bis_skin_checked="1">
+                <div class="owl-carousel owl-theme" bis_skin_checked="1">
+                    @forelse(($homeRooms ?? collect()) as $room)
+                        @php
+                            $bedAmenity  = $room->amenities->first(fn($a) => preg_match('/\blits?\b/i', $a->title));
+                            $bathAmenity = $room->amenities->first(fn($a) => preg_match('/salle\s*de\s*bain|bathroom/i', $a->title));
+                        @endphp
+                    <div class="item" bis_skin_checked="1">
+                        <div class="img" bis_skin_checked="1"><img src="{{ media_url($room->main_image ?? null, 'themes/lasanta/img/restaurant/1.jpg') }}" class="img-fluid" alt="{{ method_exists($room, 't') ? $room->t('title') : $room->title }}"></div>
+                        @if(!empty($room->discount)) <span class="discount"><i class="fa-light fa-badge-percent"></i> {{ $room->discount }}%</span>@endif
+                        <div class="cont" bis_skin_checked="1">
+                            <div class="title" bis_skin_checked="1"><a href="{{ route('rooms.show', $room->slug) }}">{{ method_exists($room, 't') ? $room->t('title') : $room->title }}</a></div>
+                            <div class="details" bis_skin_checked="1"> @if($bedAmenity)<span><i class="fa-thin fa-bed-front"></i> {{ method_exists($bedAmenity, 't') ? $bedAmenity->t('title') : $bedAmenity->title }}</span>@endif @if($bathAmenity)<span><i class="fa-thin fa-bath"></i>{{ method_exists($bathAmenity, 't') ? $bathAmenity->t('title') : $bathAmenity->title }}</span>@endif @if(!empty($room->external_id))<span><i class="fa-thin fa-expand"></i> {{ $room->external_id }} m²</span>@endif </div>
+                        </div>
+                    </div>
+                    @empty
+                    <div class="item" bis_skin_checked="1">
+                        <div class="cont" bis_skin_checked="1">
+                            <div class="title" bis_skin_checked="1"><a href="#">Aucun appartement publié</a></div>
+                            <div class="details" bis_skin_checked="1"></div>
+                        </div>
+                    </div>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </section>
+
